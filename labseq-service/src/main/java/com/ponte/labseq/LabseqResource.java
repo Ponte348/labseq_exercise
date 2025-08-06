939d9b0408b5bea57963cf2dcdc4404d0da23e93
Ponte348 <ponte@ua.pt>
2025-08-06 18:54:14 +0100

    endpoint done, I'll do the rest tomorrow (containerization, frontend, exec instructions)

--- labseq-service/src/main/java/com/ponte/labseq/LabseqResource.java
+++ labseq-service/src/main/java/com/ponte/labseq/LabseqResource.java
@@ -1,12 +1,20 @@
 package com.ponte.labseq;
 
+import java.math.BigInteger;
+
+import io.quarkus.cache.CacheResult;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/labseq")
 public class LabseqResource {
+
+    @Inject
+    LabseqService labseqService;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -14,5 +22,18 @@ public class LabseqResource {
         return "Welcome to the labseq exercise.";
     }
 
-    
+    @GET
+    @Path("/{n}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @CacheResult(cacheName = "labseqCache")
+    public Response getSequence(Integer n) {
+        try {
+            BigInteger result = labseqService.sequence(n);
+            return Response.ok(result).build();
+        } catch (IllegalArgumentException e) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                           .entity("Input must be a non-negative integer.")
+                           .build();
+        }
+    }
 }
