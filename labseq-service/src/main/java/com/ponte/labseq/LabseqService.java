8ea161d7373e5027640e41c79c53223130b9820e
Ponte348 <ponte@ua.pt>
2025-08-06 18:30:18 +0100

    basic slow logic

--- labseq-service/src/main/java/com/ponte/labseq/LabseqService.java
+++ labseq-service/src/main/java/com/ponte/labseq/LabseqService.java
@@ -12,8 +12,15 @@ public class LabseqService {
      * @return l(n) = l(n-4) + l(n-3) 
      */
     public Integer sequence(Integer a){
-        // TODO: Implement the sequence logic
-        return null;
+        if (a < 0) {
+            throw new IllegalArgumentException("Input must be a non-negative integer.");
+        }
+        if (a == 0) return 0;
+        if (a == 1) return 1;
+        if (a == 2) return 0;
+        if (a == 3) return 1;
+
+        return sequence(a - 4) + sequence(a - 3);
     }
 
 }
